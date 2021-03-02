95a4c0d48a4ef37e2f09f74380266d2b90b91a31
nastya <nastya.vergeeva@mail.ru>
2021-03-02 16:35:41 +0500

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,150 @@
+﻿
+namespace ТреугольныеМассивы
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Column8 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column7 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column8,
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4,
+            this.Column5,
+            this.Column6,
+            this.Column7});
+            this.dataGridView1.Enabled = false;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(799, 259);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // Column8
+            // 
+            this.Column8.HeaderText = "   ";
+            this.Column8.MinimumWidth = 6;
+            this.Column8.Name = "Column8";
+            this.Column8.Width = 110;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Челябинск";
+            this.Column1.MinimumWidth = 6;
+            this.Column1.Name = "Column1";
+            this.Column1.Width = 70;
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Магнитогорск";
+            this.Column2.MinimumWidth = 6;
+            this.Column2.Name = "Column2";
+            this.Column2.Width = 80;
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Миасс";
+            this.Column3.MinimumWidth = 6;
+            this.Column3.Name = "Column3";
+            this.Column3.Width = 70;
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "Златоуст";
+            this.Column4.MinimumWidth = 6;
+            this.Column4.Name = "Column4";
+            this.Column4.Width = 70;
+            // 
+            // Column5
+            // 
+            this.Column5.HeaderText = "Снежинск";
+            this.Column5.MinimumWidth = 6;
+            this.Column5.Name = "Column5";
+            this.Column5.Width = 70;
+            // 
+            // Column6
+            // 
+            this.Column6.HeaderText = "Троицк";
+            this.Column6.MinimumWidth = 6;
+            this.Column6.Name = "Column6";
+            this.Column6.Width = 70;
+            // 
+            // Column7
+            // 
+            this.Column7.HeaderText = "Аша";
+            this.Column7.MinimumWidth = 6;
+            this.Column7.Name = "Column7";
+            this.Column7.Width = 50;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(823, 287);
+            this.Controls.Add(this.dataGridView1);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "Form1";
+            this.Text = "Расстояние между городами";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column8;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column6;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column7;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ТреугольныеМассивы
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            int[][] a; // Описываем a как двумерный массив;
+            a = new int[6][];
+            for (int i = 0; i < 6; i++)
+            {
+                a[i] = new int[i + 1];
+            }
+
+            a[0][0] = 309;
+            a[1][0] = 106;
+            a[1][1] = 257;
+            a[2][0] = 142;
+            a[2][1] = 283;
+            a[2][2] = 40;
+            a[3][0] = 130;
+            a[3][1] = 439;
+            a[3][2] = 137;
+            a[3][3] = 167;
+            a[4][0] = 136;
+            a[4][1] = 222;
+            a[4][2] = 204;
+            a[4][3] = 240;
+            a[4][4] = 281;
+            a[5][0] = 330;
+            a[5][1] = 440;
+            a[5][2] = 246;
+            a[5][3] = 199;
+            a[5][4] = 362;
+            a[5][5] = 430;
+
+            String[] column0 = { "Челябинск", "Магнитогорск" , "Миасс","Златоуст","Снежинск", "Троицк","Аша" };
+            for (int i = 0; i < column0.Length; i++)
+            {
+                dataGridView1.Rows.Add(column0[i]);
+            }
+
+            for (int i = 0; i < 6; i++)
+            {
+                for (int j = 0; j < i + 1; j++)
+                {
+                    dataGridView1.Rows[i + 1].Cells[j + 1].Value = a[i][j];
+
+                    if (i == j)
+                    {
+                        dataGridView1.Rows[i].Cells[j + 1].Value = "-";
+                    }
+                }
+            }
+
+            dataGridView1.Rows[6].Cells[7].Value = "-";
+
+            int[][] b; 
+            b = new int[6][];
+            for (int i = 0; i < 6; i++)
+            {
+                b[i] = new int[6-i];
+            }
+
+            b[5][0] = 430;
+            b[4][0] = 281;
+            b[4][1] = 362;
+            b[3][0] = 167;
+            b[3][1] = 240;
+            b[3][2] = 199;
+            b[2][0] = 40;
+            b[2][1] = 137;
+            b[2][2] = 204;
+            b[2][3] = 246;
+            b[1][0] = 257;
+            b[1][1] = 283;
+            b[1][2] = 439;
+            b[1][3] = 222;
+            b[1][4] = 440;
+            b[0][0] = 309;
+            b[0][1] = 106;
+            b[0][2] = 142;
+            b[0][3] = 130;
+            b[0][4] = 136;
+            b[0][5] = 330;
+            int counter = 0;
+            for (int i = 0; i < 6; i++)
+            {
+                for (int j = 0; j < 6-i; j++)
+                {
+                    dataGridView1.Rows[i].Cells[j+counter + 2].Value = b[i][j];
+                }
+                counter++;
+            }
+        }
+    }
+}
+
